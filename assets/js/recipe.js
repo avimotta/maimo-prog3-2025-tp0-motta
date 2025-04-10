7e7691643f06d60592e3bb098c8d001af6821f4c
avi <avrilmotta@gmail.com>
2025-04-10 17:15:33 -0300

    "css y codigo"

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,24 +1,23 @@
-​​const main = async () => {
+const main = async () => {
     const searchParams = new URLSearchParams(window.location.search);
     const id = searchParams.get("id");
 
-    const $content = document.querySelector('.content');
+    const container = document.getElementById("recipe-container");
 
-    const response = await fetch(`https://dummyjson.com/recipe/${id}`);
+    const response = await fetch(`https://dummyjson.com/recipes/${id}`);
     const recipe = await response.json();
 
-
-    $content.innerHTML = `
-            <h2>${recipe.name}</h2>
-            <img src="${recipe.image}" alt="${recipe.name}" />
-            <p><strong>Tiempo de preparación:</strong> ${recipe.prepTimeMinutes} minutos</p>
-            <p><strong>Ingredientes:</strong></p>
-            <ul>
-                ${recipe.ingredients.map(ing => `<li>${ing}</li>`).join("")}
-            </ul>
-            <p><strong>Instrucciones:</strong></p>
-            <p>${recipe.instructions}</p>
-        `;
+    container.innerHTML = `
+        <h2>${recipe.name}</h2>
+        <img src="${recipe.image}" alt="${recipe.name}" />
+        <p><strong>Tiempo de preparación:</strong> ${recipe.prepTimeMinutes} minutos</p>
+        <p><strong>Ingredientes:</strong></p>
+        <ul>
+            ${recipe.ingredients.map(ing => `<li>${ing}</li>`).join("")}
+        </ul>
+        <p><strong>Instrucciones:</strong></p>
+        <p>${recipe.instructions}</p>
+    `;
 };
 
 main();
